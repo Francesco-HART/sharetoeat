9c9bb9b0621d1e0eaf3b34ccd2a4814595192673
Maxime Le Duc <maximeld19@gmail.com>
2025-07-18 11:52:57 +0200

    Detache docker

--- backend/runDockerForDev.sh
+++ backend/runDockerForDev.sh
@@ -1,4 +1,4 @@
 rm -rf ./docker-postgresql-data-for-dev
 docker-compose -f ./docker-compose-dev.yaml down --volumes
 docker-compose -f ./docker-compose-dev.yaml down --remove-orphans
-docker-compose -f ./docker-compose-dev.yaml up
+docker-compose -f ./docker-compose-dev.yaml up -d
